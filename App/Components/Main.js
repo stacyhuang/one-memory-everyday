c7d5aa57950407586b57df0367bef48d8dd60c94
Stacy Huang <stacy.y.huang@gmail.com>
2016-03-22 16:06:50 -0700

    Add new photo functionality

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -9,11 +9,12 @@ import React, {
   TouchableHighlight
 } from 'react-native';
 
+var ImagePickerManager = require('NativeModules').ImagePickerManager;
+import moment from 'moment';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Note from './Note';
 import api from '../Utils/api';
 import Separator from './Helpers/Separator';
-
 
 class Main extends Component {
   constructor(props) {
@@ -21,6 +22,7 @@ class Main extends Component {
     this.ds = new ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2});
     this.state = {
       dataSource: this.ds.cloneWithRows([]),
+      imageSource: null
     }
   }
 
@@ -40,9 +42,56 @@ class Main extends Component {
         }
       })
       .catch((error) => {
-        debugger
         console.log('Error loading memories', error);
       })
+  }
+
+  launchPicker() {
+    var options = {
+      title: 'Select Photo', // specify null or empty string to remove the title
+      cancelButtonTitle: 'Cancel',
+      takePhotoButtonTitle: 'Take Photo...', // specify null or empty string to remove this button
+      chooseFromLibraryButtonTitle: 'Choose from Library...', // specify null or empty string to remove this button
+      cameraType: 'back', // 'front' or 'back'
+      mediaType: 'photo', // 'photo' or 'video'
+      videoQuality: 'high', // 'low', 'medium', or 'high'
+      durationLimit: 10, // video recording max time in seconds
+      maxWidth: 100, // photos only
+      maxHeight: 100, // photos only
+      aspectX: 2, // android only - aspectX:aspectY, the cropping image's ratio of width to height
+      aspectY: 1, // android only - aspectX:aspectY, the cropping image's ratio of width to height
+      quality: 0.2, // 0 to 1, photos only
+      angle: 0, // android only, photos only
+      allowsEditing: false, // Built in functionality to resize/reposition the image after selection
+      noData: false, // photos only - disables the base64 `data` field from being generated (greatly improves performance on large photos)
+      storageOptions: { // if this key is provided, the image will get saved in the documents directory on ios, and the pictures directory on android (rather than a temporary directory)
+        skipBackup: true, // ios only - image will NOT be backed up to icloud
+        path: 'images' // ios only - will save image at /Documents/images rather than the root
+      }
+    };
+
+    ImagePickerManager.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      }
+      else if (response.error) {
+        console.log('ImagePickerManager Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        const source = {uri: response.uri.replace('file://', ''), isStatic: true};
+
+        this.setState({
+          imageSource: source
+        });
+
+        this.handleNewPhoto();
+      }
+    });
   }
 
   handleNewNote() {
@@ -54,7 +103,22 @@ class Main extends Component {
   }
 
   handleNewPhoto() {
-    //
+    var currentDate = moment().format("YYYY-MM-DD");
+    var memory = {
+      date: currentDate,
+      type: 'photo',
+      image_url: this.state.imageSource.uri,
+      text: 'Some note'
+    };
+
+    api.addMemory(memory)
+      .then((res) => {
+        this.getMemories();
+      })
+      .catch((error) => {
+        console.log('Request failed', error);
+        this.setState({ error });
+      });
   }
 
   renderRow(rowData) {
@@ -83,7 +147,7 @@ class Main extends Component {
         </TouchableHighlight>
         <TouchableHighlight
           style={styles.button}
-          onPress={this.handleNewPhoto.bind(this)}
+          onPress={this.launchPicker.bind(this)}
           underlayColor='#88D4F5'>
             <Icon name="camera-retro" style={styles.buttonText}/>
         </TouchableHighlight>
